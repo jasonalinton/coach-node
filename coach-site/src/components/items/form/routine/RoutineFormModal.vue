14a3fab5035eb3cdc1a454b6173e400afbcbd991
Jason <jasonalinton@gmail.com>
2023-09-26 13:08:05 -0400

    - Fade recommended iterations - Show 'Morning Joy' events

--- coach-site/src/components/items/form/routine/RoutineFormModal.vue
+++ coach-site/src/components/items/form/routine/RoutineFormModal.vue
@@ -6,7 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div v-if="!mapper.isShown" class="container">
+                <div v-if="!mapper.isShown" class="container-fluid">
                     <div class="row g-2">
                         <div class="col-12">
                             <input id="text" class="textbox" type="text" placeholder="Title"
