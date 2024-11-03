89a3ad220eb0f63022db68d1b64ab2c769b05666
Jason <jasonalinton@gmail.com>
2024-11-03 14:33:52 -0500

    Update API URL for Azure

--- coach-site/src/api/api.js
+++ coach-site/src/api/api.js
@@ -1,4 +1,4 @@
 // export const URL = "http://192.168.1.129:7104"; // Home network
 // export const URL = "http://172.20.10.11:7104"; // Hotspot
-export const URL = "http://localhost:7104"
-// export const URL = "https://coach-eea3hnh7fje3gxhw.canadaeast-01.azurewebsites.net/"
+//export const URL = "http://localhost:7104"
+export const URL = "coach-eea3hnh7fje3gxhw.canadacentral-01.azurewebsites.net"
