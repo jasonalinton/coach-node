2fdc3d5ac2376d8e8161ed6e647555ae08fed3be
Jason <jasonalinton@gmail.com>
2025-07-19 11:13:52 -0400

    Add editor JS

---
+++ coach-site/src/components/controls/input/EditorJS.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="editorjs" ref="htmlelement"></div>
+</template>
+
+<script>
+import EditorJS from '@editorjs/editorjs';
+import EmbedTool from '@editorjs/embed';
+import ListTool from '@editorjs/list';
+import ImageTool from '@editorjs/image';
+import VideoTool from './editorjs/video.js';
+
+export default {
+    name: '',
+    components: {  },
+    props: {
+        modelValue: {
+            type: [Object, String],
+            default: null,
+        },
+        placeholder: String,
+        readOnly: Boolean,
+    },
+    data: function () {
+        return {
+            htmlelement: null,
+            editor: undefined,
+            updatingModel: false
+        }
+    },
+    mounted() {
+
+    },
+    unmounted() {
+       
+    },
+    methods: {
+        
+    },
+    watch() {
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
