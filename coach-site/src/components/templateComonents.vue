fb96dbce03bdbaa7d45d07d37514cced1c21c356
Jason <jasonalinton@gmail.com>
2026-03-01 10:54:02 -0500

    Add radio to template

--- coach-site/src/components/templateComonents.vue
+++ coach-site/src/components/templateComonents.vue
@@ -1,20 +1,25 @@
 <template>
     <div>
+        <!-- Button -->
         <button type="button" class="btn btn-primary mb-2" @click="logAllSets">Log All Sets</button>
         
+        <!-- Textbox -->
         <input class="textbox text" type="text" placeholder="Title"
                v-model.lazy.trim="newPost.title" 
                v-on:keyup.enter="addTask(false)"
                v-on:keyup.esc="newTaskText = undefined"
                spellcheck="true"/>
 
+        <!-- Date-Time -->
         <input class="mb-2" type="datetime-local" :value="newPost.datetime" @change="onChange"/>
         
+        <!-- Text Area -->
         <textarea class="textarea" 
                 v-model.lazy.trim="description.value"
                 placeholder="Click to add Description"
                 spellcheck="true"></textarea>
 
+        <!-- Header -->
         <div class="top d-flex flex-column sticky-top">
             <div class="label d-flex flex-row mb-2">
                 <img class="icon-button mb-2"
@@ -27,7 +32,16 @@
             <input id="name" class="textbox mb-2" type="text" ref="text"  placeholder="Workout Name"
                     v-model.lazy.trim="name.value" 
                     spellcheck="true"/>
-            </div>
+        </div>
+
+        <!-- Radio -->
+        <div class="app-pills d-flex flex-row justify-content-between mt-1 gap-1">
+            <span v-for="timeframe in timeframes.slice(0,3)" v-bind:key="timeframe.id" 
+                    class="app-pill flex-grow-1" :class="{ 'selected': isTimeframeSelected(timeframe.id)}"
+                    @click="toggleTimeframe(timeframe.id)">
+                {{ timeframe.text }}
+            </span>
+        </div>
     </div>
 </template>
 
@@ -53,3 +67,25 @@ function getDateTimeString() {
 } 
 
 </script>
+
+<style>
+.pill {
+    cursor: pointer;
+    padding: 4px 0px;
+    line-height: 14px;
+    font-size: 14px;
+    background-color: var(--pill-default);
+    border-radius: 4px;
+    border: transparent solid 1px;
+}
+.pill:hover {
+    border: var(--pill-border-hover) solid 1px;
+}
+.pill.selected {
+    border: var(--pill-border-hover) solid 1px;
+    background-color: var(--pill-background-selected);
+}
+.pill:active {
+    background-color: var(--pill-background-selected);
+}
+</style>
