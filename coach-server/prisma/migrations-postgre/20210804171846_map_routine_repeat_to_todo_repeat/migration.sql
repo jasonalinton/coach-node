2d4e4cb6d0eeeff1e10c16a9315f63f5419a9945
Jason <jasonalinton@gmail.com>
2026-06-14 14:02:28 -0400

    Add coach-server to gitignore

--- coach-server/prisma/migrations-postgre/20210804171846_map_routine_repeat_to_todo_repeat/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Repeat" ADD COLUMN     "idRoutineRepeat" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Repeat" ADD FOREIGN KEY ("idRoutineRepeat") REFERENCES "Repeat"("id") ON DELETE SET NULL ON UPDATE CASCADE;
